5903c9337998946ca7c4f09fe89dbf8da5d1c0ca
aman-109 <aman.mate.52@gmail.com>
2022-12-19 20:32:44 +0530

    second

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,15 @@ require("dotenv").config()
 const mongoose=require("mongoose")
 
 const connect=()=>{
-    return mongoose.connect(process.env.MONGODB_URL)
+    return mongoose.connect(process.env.MONGODB_URL,{ 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+        },(err) => {
+        if (err) {
+        console.log("error in connection");
+        } else {
+        console.log("mongodb is connected");
+        }})
 }
 
 
